d2d83a8844eda0fbd4f2bf26cd03c3ac9b9a9b84
Anderson Lopes <andersonlopes017.al@gmail.com>
2024-03-08 02:24:54 -0400

    ✨ inserindo comentários

--- src/components/Comment/index.jsx
+++ src/components/Comment/index.jsx
@@ -2,7 +2,7 @@ import styles from './Comment.module.css';
 
 import { ThumbsUp, Trash } from 'phosphor-react';
 
-export function Comment() {
+export function Comment({ content }) {
   return (
     <div className={styles.comment}>
       <img src="https://github.com/lopesA95.png" alt="" />
@@ -20,7 +20,7 @@ export function Comment() {
             </button>
           </header>
 
-          <p>Muito bom Devon, parabéns!! 👏👏</p>
+          <p>{content}</p>
         </div>
 
         <footer>
--- src/components/Post/index.jsx
+++ src/components/Post/index.jsx
@@ -6,7 +6,9 @@ import { Comment } from '../Comment';
 import styles from './Post.module.css';
 
 export function Post({ author, publishedAt, content }) {
-  const [comments, setComments] = useState([1, 2,]);
+  const [comments, setComments] = useState(['Post muito bacana, hein?!']);
+
+  const [newCommentText, setNewCommentText] = useState('');
 
   const pusblishedDateFormated = format(publishedAt, "d 'de' LLLL 'às' HH:mm'h'", { locale: ptBr, })
 
@@ -15,7 +17,12 @@ export function Post({ author, publishedAt, content }) {
   function handleCreateNewComment(e) {
     e.preventDefault()
 
-    setComments([...comments, comments.length + 1]);
+    setComments([...comments, newCommentText]);
+    setNewCommentText('');
+  }
+
+  function handleNewCommentChange() {
+    setNewCommentText(event.target.value)
   }
 
   return (
@@ -46,7 +53,10 @@ export function Post({ author, publishedAt, content }) {
         <strong>Deixe seu feedback</strong>
 
         <textarea
+          name='comment'
           placeholder="Deixe um comentário"
+          value={newCommentText}
+          onChange={handleNewCommentChange}
         />
 
         <footer>
@@ -56,7 +66,7 @@ export function Post({ author, publishedAt, content }) {
 
       <div className={styles.commentList}>
         {comments.map(comment => {
-          return <Comment />
+          return <Comment content={comment} />
         })}
       </div>
     </article>
